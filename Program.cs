57fc7380a2a3750c1974e58134f018c842e7069b
Daniela Hinostroza <daniela.hinostroza.946@gmail.com>
2026-09-23 09:42:24 -0500

    Corregido calculo de promedio e indice del bucle

--- Program.cs
+++ Program.cs
@@ -1,8 +1,23 @@
-﻿int[] notas = {15, 18, 12, 17, 20};
+﻿//Funcion de imprimir notas
+static void ImprimirNotas(int[] notas)
+{
+    for(int i=0; i<notas.Length; i++)
+    {
+        Console.WriteLine(notas[i]);
+    }
+}
 
-//Imprimir las notas 
+
+int[] notas = { 15, 18, 12, 17, 20};
+
+//Imprimir las notas
+ImprimirNotas(notas);
+
+//Calcular el promedio de las notas
+int suma=0;
 for(int  i=0; i<notas.Length; i++)
 {
     Console.WriteLine(notas[i]);
 }
-
+double promedio = (double)suma / notas.Length;
+Console.WriteLine($"El promedio es:{promedio}");
